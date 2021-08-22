ee423330ef05871af98d3e2cde3f148c39230a86
Brijesh Rana <brijesh.1995rana@gmail.com>
2021-08-22 15:24:08 +0530

    server code

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://brijnewest:mVQYwBQLNRVEdKAd@cluster1.mwo0p.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
